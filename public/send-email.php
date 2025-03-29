1cc92e1987663262200af9a90c094155dd0b084a
Paul Gorman <Paul.D.Gorman@gmail.com>
2025-03-29 23:17:29 +0000

    WIP - send emails

---
+++ public/send-email.php
@@ -0,0 +1,31 @@
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Email configuration
+    $to = "paul.d.gorman+ripets@gmail.com"; // Replace with your email address
+    $subject = "New Contact Form Submission";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Email body
+    $body = "You have received a new message from your website contact form:\n\n";
+    $body .= "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Message:\n$message\n";
+
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you! Your message has been sent.";
+    } else {
+        echo "Sorry, there was an error sending your message. Please try again later.";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
