74e40cb7aebc59223be1f52d24a5b8d28251464e
Paul Gorman <Paul.D.Gorman@gmail.com>
2025-04-01 22:02:35 +0100

    formatted

--- public/scripts/modalHandler.js
+++ public/scripts/modalHandler.js
@@ -1,28 +1,28 @@
 const modals = {
-    contact: {
-      button: document.getElementById("contactButton"),
-      modal: document.getElementById("contactFormModal"),
-      close: document.getElementById("closeModal"),
-    },
-    about: {
-      button: document.getElementById("aboutButton"),
-      modal: document.getElementById("aboutModal"),
-      close: document.getElementById("closeAboutModal"),
-    },
-  };
+  contact: {
+    button: document.getElementById("contactButton"),
+    modal: document.getElementById("contactFormModal"),
+    close: document.getElementById("closeModal"),
+  },
+  about: {
+    button: document.getElementById("aboutButton"),
+    modal: document.getElementById("aboutModal"),
+    close: document.getElementById("closeAboutModal"),
+  },
+};
 
-  Object.values(modals).forEach(({ button, modal, close }) => {
-    button.addEventListener("click", () => {
-      modal.style.display = "flex";
-    });
-
-    close.addEventListener("click", () => {
-      modal.style.display = "none";
-    });
-
-    window.addEventListener("click", (event) => {
-      if (event.target === modal) {
-        modal.style.display = "none";
-      }
-    });
+Object.values(modals).forEach(({ button, modal, close }) => {
+  button.addEventListener("click", () => {
+    modal.style.display = "flex";
   });
+
+  close.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
